2462fd1f841b140e93385aa16cef9cad773231ad
murggel <murggel@web.de>
2024-05-01 10:25:01 +0200

    option to show only empty variables

--- main/src/main/java/cgeo/geocaching/ui/VariableListView.java
+++ main/src/main/java/cgeo/geocaching/ui/VariableListView.java
@@ -2,6 +2,7 @@ package cgeo.geocaching.ui;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.databinding.VariableListViewBinding;
+import cgeo.geocaching.settings.Settings;
 import cgeo.geocaching.ui.dialog.SimpleDialog;
 import cgeo.geocaching.ui.recyclerview.ManagedListAdapter;
 import cgeo.geocaching.utils.Log;
@@ -262,6 +263,19 @@ public class VariableListView extends LinearLayout {
             this.visibleVariables.addAll(newVisibleVariables);
             this.setFilter(d -> this.visibleVariables.contains(d.getVar()), true);
             filterEnabled = true;
+        }
+
+        public void setVisibleVariables() {
+            //disable filter
+            this.filterEnabled = Settings.getHideCompletedVariables();
+            this.visibleVariables.clear();
+            this.setFilter(d -> filterEnabled ? !isVariableComplete(d.getVar()) : true, true);
+        }
+
+        private boolean isVariableComplete(final String var) {
+            // return StringUtils.isNotEmpty(this.variables.getState(var).getFormulaString();
+            return null != this.variables.getValue(var)
+                    && this.variables.getState(var).getState() == VariableMap.State.OK;
         }
 
         public void ensureVariables(final Collection<String> variables) {
