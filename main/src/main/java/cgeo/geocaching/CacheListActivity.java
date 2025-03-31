a40d38d99cd04a786ac0e0a557083edd05e122e1
murggel <murggel@web.de>
2025-03-31 22:44:35 +0200

    Remove from other lists

--- main/src/main/java/cgeo/geocaching/CacheListActivity.java
+++ main/src/main/java/cgeo/geocaching/CacheListActivity.java
@@ -621,6 +621,8 @@ public class CacheListActivity extends AbstractListActivity implements FilteredA
             setMenuItemLabel(menu, R.id.menu_show_attributes, R.string.caches_show_attributes, R.string.caches_show_attributes);
             MenuUtils.setEnabled(menu, R.id.menu_set_cache_icon, !isEmpty);
             setMenuItemLabel(menu, R.id.menu_set_cache_icon, R.string.caches_set_cache_icon, R.string.caches_set_cache_icon);
+            MenuUtils.setVisibleEnabled(menu, R.id.menu_remove_from_other_lists, isOffline && listId != PseudoList.ALL_LIST.id, !isEmpty);
+            setMenuItemLabel(menu, R.id.menu_remove_from_other_lists, R.string.caches_remove_from_other_lists, R.string.caches_remove_from_other_lists_all);
 
             // Manage Lists submenu
             MenuUtils.setVisible(menu, R.id.menu_lists, isOffline);
@@ -811,9 +813,8 @@ public class CacheListActivity extends AbstractListActivity implements FilteredA
             invalidateOptionsMenuCompatible();
         } else if (menuItem == R.id.menu_show_attributes) {
             adapter.showAttributes(adapter.getCheckedOrAllCaches());
-        } else if (menuItem == R.id.menu_make_list_unique) {
-            new MakeListUniqueCommand(this, listId) {
-
+        } else if (menuItem == R.id.menu_make_list_unique || menuItem == R.id.menu_remove_from_other_lists) {
+            new MakeListUniqueCommand(this, listId, Geocache.getGeocodes(menuItem == R.id.menu_remove_from_other_lists ? adapter.getCheckedOrAllCaches() : new ArrayList<>())) {
                 @Override
                 protected void onFinished() {
                     refreshActionBarTitle();
@@ -823,7 +824,6 @@ public class CacheListActivity extends AbstractListActivity implements FilteredA
                 protected void onFinishedUndo() {
                     refreshActionBarTitle();
                 }
-
             }.execute();
         } else if (menuItem == R.id.menu_upload_bookmarklist) {
             BookmarkUtils.askAndUploadCachesToBookmarkList(this, adapter.getCheckedOrAllCaches());
